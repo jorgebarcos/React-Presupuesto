efddd093abe7de45b5a75e65869f7a6330dca9f0
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-24 18:57:56 -0300

    Agregando el Primer Componente

---
+++ src/components/Pregunta.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+
+function Pregunta() {
+	return (
+		<Fragment>
+			<h2>Coloca tu Presupuesto</h2>
+			<form>
+				<input
+					type="number"
+					className="u-full-width"
+					placeholder="Agrega tu Presupuesto"
+					//onChange={}
+				/>
+				<input type="submit" className="button-primary u-full-width" value="Definir Presupuesto" />
+			</form>
+		</Fragment>
+	);
+}
+export default Pregunta;
