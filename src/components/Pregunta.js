d88ebe38b1fab919efb8f37f6c46aa4af19e2050
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-24 19:18:46 -0300

    Validando el Presupuesto

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,15 +1,31 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 function Pregunta() {
+	//definir el state
+	const [ cantidad, guardarCantidad ] = useState(0);
+	const [ error, guardarError ] = useState(false);
+
+	// validar el presupuesto
+	const agregarPresupuesto = (e) => {
+		e.preventDefault();
+
+		// validar
+		if (cantidad < 1 || isNaN(cantidad)) {
+			guardarError(true);
+			return;
+		}
+
+		// Si se pasa la validación
+	};
 	return (
 		<Fragment>
 			<h2>Coloca tu Presupuesto</h2>
-			<form>
+			<form onSubmit={agregarPresupuesto}>
 				<input
 					type="number"
 					className="u-full-width"
 					placeholder="Agrega tu Presupuesto"
-					//onChange={}
+					onChange={(e) => guardarCantidad(parseInt(e.target.value, 10))}
 				/>
 				<input type="submit" className="button-primary u-full-width" value="Definir Presupuesto" />
 			</form>
