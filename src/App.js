e26ff94f771ba6b2df6ef5168c1ed8fbcd301a65
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-25 16:00:54 -0300

    Leyendo los Valores de los Gastos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Pregunta from './components/Pregunta';
-
+import Formulario from './components/Formulario';
 function App() {
 	// state
 	const [ presupuesto, guardarPresupuesto ] = useState(0);
@@ -19,7 +19,7 @@ function App() {
 					) : (
 						<div className="row">
 							<div className="one-half column">
-								<p>Formulario aqui</p>
+								<Formulario />
 							</div>
 							<div className="one-half column" />
 						</div>
---
+++ src/components/Formulario.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+function Formulario(props) {
+	//state
+	const [ nombreGasto, guardarNombreGasto ] = useState('');
+	const [ cantidadGasto, guardarCantidadGasto ] = useState(0);
+	const [ error, guardarError ] = useState(false);
+
+	// Cuando se agrega el gasto
+	const agregarGasto = (e) => {
+		e.preventDefault();
+
+		// validar
+		if (cantidadGasto < 1 || isNaN(cantidadGasto) || nombreGasto === '') {
+			guardarError(true);
+			return;
+		}
+
+		// pasar el gasto al componente principal
+	};
+
+	return (
+		<form onSubmit={agregarGasto}>
+			<h2>Agrega tus Gastos Aquí</h2>
+
+			<div className="campo">
+				<label>Nombre Gasto</label>
+				<input
+					type="text"
+					className="u-full-width"
+					placeholder="Ej. Transporte"
+					onChange={(e) => guardarNombreGasto(e.target.value)}
+				/>
+			</div>
+
+			<div className="campo">
+				<label>Cantidad Gasto</label>
+				<input
+					type="number"
+					className="u-full-width"
+					placeholder="Ej. 300"
+					onChange={(e) => guardarCantidadGasto(parseInt(e.target.value, 10))}
+				/>
+			</div>
+
+			<input type="submit" className="button-primary u-full-width" value="Agregar Gasto" />
+		</form>
+	);
+}
+export default Formulario;
